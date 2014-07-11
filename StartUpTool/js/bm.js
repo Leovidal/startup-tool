197e429e98e1fa882125365ebbefde7bbdc0de59
Hristo Valyavicharski <h.valyavicharski@gmail.com>
2014-07-11 20:45:58 +0300

    ADD: index.html file

---
+++ StartUpTool/js/bm.js
@@ -0,0 +1,57 @@
+angular.module('bmc', ['ngStorage']);
+
+function MainController($scope, $localStorage) {
+  $scope.text = '';
+  $scope.bmcontainer = "key-partners";
+
+  $scope.$storage = $localStorage.$default({
+     items : [{container: "key-partners", data: []},
+        {container: "key-activities", data: []},
+        {container: "key-resources", data: []},
+        {container: "value-proposition", data: []},
+        {container: "customer-relationships", data: []},
+        {container: "channels", data: []},
+        {container: "customer-segment", data: []},
+        {container: "cost-structure", data: []},
+        {container: "revenue-streams", data: []}]
+    });
+
+  $scope.init = function () {
+      debugger
+    if (localStorage.getItem("bm")) {
+        $scope.items = JSON.parse(localStorage.getItem("bm"));
+    }
+    else {
+    var bm = new Array();
+        bm = [{container: "key-partners", data: []},
+        {container: "key-activities", data: []},
+        {container: "key-resources", data: []},
+        {container: "value-proposition", data: []},
+        {container: "customer-relationships", data: []},
+        {container: "channels", data: []},
+        {container: "customer-segment", data: []},
+        {container: "cost-structure", data: []},
+        {container: "revenue-streams", data: []}];
+
+        //Create the LocalStorate Item
+        localStorage.setItem("bm", JSON.stringify(bm));
+	}
+  }
+
+    //$scope.init();
+	$scope.add = function () {
+		
+		angular.forEach($scope.$storage.items, function(item) {
+			if (item.container == $scope.bmcontainer) item.data.push({text: $scope.text, status: true});
+		});
+	};
+
+    $scope.remove = function (index, arrayIndex) {
+        $scope.$storage.items[arrayIndex].data.splice(index, 1);
+    }
+
+    $scope.move = function () {
+
+    }
+}
+
